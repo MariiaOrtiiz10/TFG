be9e1253070d6ed5e0250211dca6dce95b6f4dd8
MariiaOrtiiz10 <mariamontewei10@gmail.com>
2025-01-14 01:14:21 +0100

    Configuration, main y pubspec

---
+++ lib/custom/configuration.dart
@@ -0,0 +1,7 @@
+class configuration {
+  static String mVersion = "1.0.0";
+  static String mAuthor = "Mariia10";
+  //Variables de server desarrollo
+  static String mSupabaseUrl = "https://memblueouodefobxlums.supabase.co";
+  static String mSupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lbWJsdWVvdW9kZWZvYnhsdW1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4MTIyNDgsImV4cCI6MjA1MjM4ODI0OH0.BAjVHH3J2yfP0YUPZ4MvpU-QZHc5KD1dynENjiTE8lQ";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:prueba_bbdd/custom/configuration.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future <void> main() async {
+  //Primero se crea la instancia de supabase. Para usar await se debe usar async, por eso pongo en main en async.
+  await Supabase.initialize(
+    url: configuration.mSupabaseUrl,
+    anonKey: configuration.mSupabaseKey,
+  );
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
